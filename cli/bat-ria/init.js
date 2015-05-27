6aeca1c33f1705d2d2ad89f7f146e12c7722aca2
chestnut <mini.chenli@gmail.com>
2015-05-27 13:24:50 +0800

    delete git2svn cli

--- cli/bat-ria/init.js
+++ cli/bat-ria/init.js
@@ -124,11 +124,10 @@ cli.main = function (args, opts) {
         require('../../lib/util/gen-webserver-config')(projectInfo);
         require('../../lib/util/gen-test-config')(projectInfo);
         require('../../lib/util/gen-default-specs')(projectInfo, options);
-        require('../../lib/util/gen-git2svn-hook')(projectInfo, options);
 
         var copies = [
-            { source: '../../asset/img', target: 'src/common/img' },
-            { source: '../../asset/css', target: 'src/common/css' }
+            {source: '../../asset/img', target: 'src/common/img'},
+            {source: '../../asset/css', target: 'src/common/css'}
         ];
         require('../../lib/util/copy')(projectInfo, copies);
 
